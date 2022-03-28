3658cf9f22ce597c4135e755343771179d7137e7
adonOFFICIAL <akunsimpann01@gmail.com>
2022-03-28 16:00:25 +0700

    ngetes doang bang

---
+++ result/lib/donasi.js
@@ -0,0 +1,22 @@
+const donasi = (pushname, prefix, botName, ownerName) => { 
+	return `Hallo, kak bantu donasi ya supaya bot lebih baik👋
+╔════════════════════
+║ *DONASI UNTUK RAMDANI BOTZ*
+╠════════════════════
+║╭──❉ *DONASI KAK* ❉─────
+║│➸ *GOPAY* : _tidak tersedia_
+║│➸ *DANA* : 089
+║│➸ *OVO* : 0895
+║│➸ *PULSA* : 082224117270
+║╰──────────────────
+╠════════════════════
+║       
+║  ▌│█║▌║▌║║▌║▌║█│▌
+║  ▌│█║▌║▌║║▌║▌║█│▌
+║        
+╠════════════════════
+║ _*© CREATOR BY ADON OFFICIAL*_
+╚════════════════════`
+}
+
+exports.donasi = donasi
---
+++ result/lib/grupbot.js
@@ -0,0 +1,13 @@
+const grupbot = (prefix) => { 
+	return `• *Grup Bot*
+*Link grup 1* : http://bit.ly/3iHgx9
+*Link grup 2* : http://bit.ly/3ibzCs
+*Link grup 3* : http://bit.ly/36l3dp
+*Link grup coding bersama* : http://bit.ly/36kz1m 
+*Link grup tes bot* : http://bit.ly/3ib0p12
+*Link grup murid bot* : http://bit.ly/3IbZB6
+*Link grup saling subscribe* : http://bit.ly/3Jh3FE
+`
+}
+
+exports.grupbot = grupbot
